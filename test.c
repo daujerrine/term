8ce2cc562215350f447256e5ff5a429e2dd1dae5
aghorui <anamitraghorui@gmail.com>
2018-11-26 08:57:54 +0530

    Cleanup

--- test.c
+++ test.c
@@ -4,6 +4,7 @@
 char *boxc[] = {
     "╔", "╗", "╚", "╝", "═", "║"
 };
+
 void box(unsigned int x, unsigned int y, unsigned int w, unsigned int h)
 {
     tm_setxy(x,y);
@@ -32,25 +33,30 @@ void box(unsigned int x, unsigned int y, unsigned int w, unsigned int h)
         tm_setxy(x + w - 1, y + 1 + i);
     }
 }
+
 int main()
 {
-    char c;
+    int w, h;
     tm_init();
     tm_cls();
+    
     tm_setxy(0,0);
     printf("Hello.");
     tm_setxy(10,2);
     printf("Hi");
+    
     tm_setxy(8,7);
-    int w, h;
+    
     tm_getwh(&w, &h);
-    printf("w:%d h:%d i:%u o:%u\n", w, h, __tm_tstructi.c_ispeed, __tm_tstruct.c_ospeed);
+    printf("width:%d height:%d", w, h);
     tm_setcan(0);
     tm_setecho( 0);
     box(6, 5, w-10, h-10);
     getchar();
+    
     tm_cls();
     tm_setxy(0,0);
     tm_reset();
+    
     return 0;
 }
